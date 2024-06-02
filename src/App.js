3a7e840043ef790b3ba3740c5ca79815cff4fbe3
OmarElati <5796@holbertonstudents.com>
2024-06-03 00:35:57 +0200

    Implement an app with React

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+import Authentication from './routes/auth/Authentication';
+import Dashboard from './routes/dashboard/Dashboard';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [userUsername, setUserUsername] = useState('');
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (!accessToken) {
+      return;
+    }
+
+    axios.post('http://localhost:8000/api/auth/', null, {
+      headers: { Authorization: `Bearer ${accessToken}`},
+    })
+    .then(response => {
+      setIsLoggedIn(true);
+      setUserUsername(response.data.username);
+    });
+  }, [userUsername]);
+
+  return (
+    <div className="App">
+      {isLoggedIn ? <Dashboard userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} /> : <Authentication setIsLoggedIn={setIsLoggedIn} setUserUsername={setUserUsername} />}
+    </div>
+  );
+}
+
+export default App;
